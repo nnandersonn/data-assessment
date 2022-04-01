1d9d3774a4e3efa7f131477e40f071276b4acbc1
Nicholas Anderson <anderson.nick@me.com>
2022-04-01 08:35:18 -0700

    city now being added and displayed

--- travel-journal/server/controller.js
+++ travel-journal/server/controller.js
@@ -1,6 +1,44 @@
+require('dotenv').config()
+const Sequelize = require('sequelize')
 
+const {CONNECTION_STRING} = process.env
+const sequelize = new Sequelize(CONNECTION_STRING, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+  })
 
 module.exports = {
+    getCountries: (req,res) => {
+        sequelize.query(`
+        SELECT * FROM countries
+        `)
+        .then(dbRes => res.status(200).send(dbRes[0]))
+        .catch(err=> console.log(err))
+    },
+
+    createCity: (req, res) => {
+        const {name, rating, countryId} = req.body
+        sequelize.query(`
+        INSERT INTO cities (name, rating, country_id)
+        VALUES ('${name}', ${rating}, ${countryId});
+        `)
+        .then(dbRes => res.status(200).send(dbRes[0]))
+        .catch(err=> console.log(err))
+    },
+
+    getCities: (req, res)=>{
+        sequelize.query(`
+        SELECT cities.city_id, cities.name AS city, cities.rating, countries.country_id, countries.name AS country FROM countries
+        JOIN cities ON countries.country_id = cities.country_id
+        `)
+        .then(dbRes => res.status(200).send(dbRes[0]))
+        .catch(err=> console.log(err))
+    },
+
     seed: (req, res) => {
         sequelize.query(`
             drop table if exists cities;
@@ -11,7 +49,12 @@ module.exports = {
                 name varchar
             );
 
-            *****YOUR CODE HERE*****
+            create table cities (
+                city_id serial primary key, 
+                name varchar,
+                rating integer,
+                country_id integer references countries(country_id)                
+            );
 
             insert into countries (name)
             values ('Afghanistan'),
